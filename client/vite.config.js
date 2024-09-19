d0000fbf95bd695d197997ec2714a811f959534c
Kamran Khan Alwi <82154070+KAMRANKHANALWI@users.noreply.github.com>
2024-09-20 00:50:08 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Load environment variables from a .env file if needed
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
+    'process.env': {
+      // Specify only the variables you need
+      REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
+      // Add any other variables you want to expose here
+    },
   },
 });
